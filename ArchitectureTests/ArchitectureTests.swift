fe4701d2cd874694772f9f907ed6a656e6522fd4
Jivan <1330336293@qq.com>
2022-02-08 13:25:12 +0800

    修改错误

--- ArchitectureTests/ArchitectureTests.swift
+++ ArchitectureTests/ArchitectureTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Architecture
 
-class SwiftBaseProjectTests: XCTestCase {
+class ArchitectureTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
