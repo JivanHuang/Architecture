70fd07295b84d7203af9d6cd3a5f25b91364109b
Jivan <1330336293@qq.com>
2022-05-23 11:05:40 +0800

    ADDED: 增加String md5加密

---
+++ Architecture/SwiftKit/StringCoder/MD5StringEncoder.swift
@@ -0,0 +1,27 @@
+//
+//  MD5StringEncoder.swift
+//  SwiftKit
+//
+//  Created by Jivan on 2022/5/23.
+//
+
+import CommonCrypto
+import Foundation
+// MD5 加密已经不在安全
+public class MD5StringEncoder: StringEncoder {
+    public func encode(_ string: String) -> String? {
+        guard let chars = string.cString(using: .utf8) else { return string }
+        let stringLegth = CUnsignedInt(string.lengthOfBytes(using: .utf8))
+        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
+        CC_MD5(chars, stringLegth, result)
+        var hash = String()
+        for i in 0 ..< digestLen {
+            hash += String(format: "%02x", result[i])
+        }
+        result.deallocate()
+        return hash
+    }
+
+    public init() {}
+}
