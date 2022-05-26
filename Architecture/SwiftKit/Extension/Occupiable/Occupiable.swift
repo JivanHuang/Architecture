40bb5a748e9a3f16373a65a32d604c9feb462c0e
Jivan <1330336293@qq.com>
2022-05-26 14:39:22 +0800

    FIXED: 删除 `Optional+IsSet.swift`, 增加 `Occupiable.swift`

---
+++ Architecture/SwiftKit/Extension/Occupiable/Occupiable.swift
@@ -0,0 +1,39 @@
+//
+//  Occupiable.swift
+//  Architecture
+//
+//  Created by Jivan on 2022/5/26.
+//
+
+import Foundation
+
+public protocol Occupiable {
+    var isEmpty: Bool { get }
+    var isNotEmpty: Bool { get }
+}
+
+public extension Occupiable {
+    var isNotEmpty: Bool {
+        !isEmpty
+    }
+}
+
+public extension Optional where Wrapped: Occupiable {
+    var isNil: Bool {
+        switch self {
+        case .none:
+            return true
+        case let .some(value):
+            return value.isEmpty
+        }
+    }
+
+    var isNonNil: Bool {
+        !isNil
+    }
+}
+
+extension String: Occupiable {}
+extension Array: Occupiable {}
+extension Dictionary: Occupiable {}
+extension Set: Occupiable {}
